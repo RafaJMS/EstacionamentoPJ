63175e80b6badbd0f04f9f3235c143b4a8dc5db6
Rafael Mecenas <rafaeljmecenas1@gmail.com>
2024-01-31 17:01:50 -0300

    Commit Estacionamento

---
+++ EstacionamentoPJ/Estacionamento.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstacionaJá
+{
+    internal class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoHora = precoHora;
+        }
+
+        public void adicionarVeiculo()
+        {
+            Console.Write("Insira a placa do veiculo que vai ser adicionado: ");
+            string placa = Console.ReadLine();
+
+            if (veiculos.Any(X => X.Equals(placa)))
+            {
+                Console.WriteLine("Essa placa já está cadastrada. Insira uma válida!");
+            }
+            else
+            {
+                veiculos.Add(placa);
+                Console.WriteLine("Veiculo cadastrado! Bem vindo ao EstacionaJá!");
+            }
+        }
+
+        public void removerVeiculo()
+        {
+            Console.Write("Insira a placa do veiculo que vai ser removido: ");
+            string placa = Console.ReadLine();
+
+
+            if (veiculos.Any(x => x.Equals(placa)))
+            {
+                Console.Write("Insira a quantidade de horas permanecidas no pátio: ");
+                decimal horas = Convert.ToDecimal(Console.ReadLine());
+                
+                decimal precoTotal = precoInicial + precoHora * horas;
+                
+                Console.WriteLine("Placa: " + placa);
+                Console.WriteLine("Horas: " + horas);
+                Console.WriteLine("Preço Hora: " + precoHora);
+                Console.WriteLine("Preço Fixo: " + precoInicial);
+                Console.WriteLine("Total: " + precoTotal);
+
+                veiculos.Remove(placa);
+                Console.WriteLine("Veiculo removido. Volte sempre!");
+                
+            }
+            else
+            {
+                Console.WriteLine("Essa placa não se encontra no estacionamento. Insira uma válida!");
+            }
+        }
+
+        public void listarVeiculos()
+        {
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veiculos estacionados são");
+                foreach (var item in veiculos)
+                {
+                    Console.WriteLine("Placa: " + item);
+                }
+            }else 
+            {
+                Console.WriteLine("Não há veiculos estacionados!");
+            }
+        }
+    }
+}
--- EstacionamentoPJ/Program.cs
+++ EstacionamentoPJ/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EstacionaJá;
+
+decimal precoInicial = 0;
+decimal precoHora = 0;
+
+Console.WriteLine("Bem vindo ao EstacioneJá");
+
+Console.Write("Insira o valor fixo: ");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Insira o valor por hora: ");
+precoHora = Convert.ToDecimal(Console.ReadLine());
+
+Estacionamento locacao1 = new (precoHora,precoInicial);
+
+bool repeat = true;
+
+while (repeat)
+{
+    
+    Console.WriteLine("-----------------------------");
+    Console.WriteLine("1 -- Adicionar Veiculo");
+    Console.WriteLine("2 -- Remover Veiculo");
+    Console.WriteLine("3 -- Listar Veiculo");
+    Console.WriteLine("4 -- Sair da aplicação");
+
+    switch (Console.ReadLine()) {  
+        
+        case "1":
+            locacao1.adicionarVeiculo();
+        break; 
+        
+        case "2":
+            locacao1.removerVeiculo();
+            break; 
+        
+        case "3":
+            locacao1.listarVeiculos();
+            break;
+        
+        case "4":
+            repeat = false;
+            break;
+        
+        default: 
+            Console.WriteLine("Insira uma opção válida!");
+            break;
+    }
+}
